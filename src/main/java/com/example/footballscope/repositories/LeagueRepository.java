df3a6544a0bcc6c8d81aa4b4ef2a23333fa5c324
alexvd5 <aleksandar_yordanov2001@abv.bg>
2026-08-05 17:44:52 +0300

    Adding @Repository annotation

--- src/main/java/com/example/footballscope/repositories/LeagueRepository.java
+++ src/main/java/com/example/footballscope/repositories/LeagueRepository.java
@@ -2,9 +2,10 @@ package com.example.footballscope.repositories;
 
 import com.example.footballscope.models.League;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface LeagueRepository extends JpaRepository<League,Long> {
     Optional<League> findByCode(String code);
 }
