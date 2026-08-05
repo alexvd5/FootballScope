ab5b325315e52565a7a219e9b6136f4d66046013
alexvd5 <aleksandar_yordanov2001@abv.bg>
2026-08-05 17:25:13 +0300

    Implementing Team model

---
+++ src/main/java/com/example/footballscope/models/Team.java
@@ -0,0 +1,27 @@
+package com.example.footballscope.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Team {
+    @Id
+    private Long id;
+
+    private String name;
+
+    //Three-Letter Abbreviation
+    private String tla;
+
+    private String badge;
+
+}
+
