d3ba82a3d1f3922b9d2d5cd8dc66a6c4dd632c89
alexvd5 <aleksandar_yordanov2001@abv.bg>
2026-08-05 22:42:47 +0300

    Adding missing annotations

--- src/main/java/com/example/footballscope/models/Match.java
+++ src/main/java/com/example/footballscope/models/Match.java
@@ -4,10 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Match {
     @Id
     private Long id;
