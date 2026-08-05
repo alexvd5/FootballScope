ad322207af0f41499da3e468aa1eb04833d234e7
alexvd5 <aleksandar_yordanov2001@abv.bg>
2026-08-05 17:20:54 +0300

    Implementing League model

---
+++ src/main/java/com/example/footballscope/models/League.java
@@ -0,0 +1,26 @@
+package com.example.footballscope.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class League {
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String country;
+
+    private String logo;
+
+    private int season;
+}
