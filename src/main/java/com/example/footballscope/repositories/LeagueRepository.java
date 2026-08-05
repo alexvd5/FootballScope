fd3e3bdf0c1cf685154bc7fe9853392d938fa386
alexvd5 <aleksandar_yordanov2001@abv.bg>
2026-08-05 17:45:20 +0300

    Fixing namespaces

--- src/main/java/com/example/footballscope/repositories/LeagueRepository.java
+++ src/main/java/com/example/footballscope/repositories/LeagueRepository.java
@@ -3,8 +3,8 @@ package com.example.footballscope.repositories;
 import com.example.footballscope.models.League;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
+
 @Repository
 public interface LeagueRepository extends JpaRepository<League,Long> {
     Optional<League> findByCode(String code);
