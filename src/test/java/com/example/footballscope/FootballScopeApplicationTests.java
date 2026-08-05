34125592be8ea421953562306afe68d2319dd658
alexvd5 <aleksandar_yordanov2001@abv.bg>
2026-08-05 17:18:03 +0300

    Initial commit

---
+++ src/test/java/com/example/footballscope/FootballScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballScopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
